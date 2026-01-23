4f8c24d32ff3f54a4ce9a66e60533405e1fb4540
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2026-01-23 17:06:49 -0300

    feat: first commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// Dependencies
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+// Components
+import App from "./App.tsx";
+
+// Styles
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
